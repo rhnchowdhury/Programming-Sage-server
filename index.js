c6ef9abe340e4637a6870ed0b8a8154bb4a69597
Raihan <rhnchdri@gmail.com>
2022-11-09 08:45:30 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,10 +9,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ajoxj5t.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
 app.get('/', (req, res) => {
     res.send('programming sage running')
 });
 
-app.listen(post, () => {
+app.listen(port, () => {
     console.log(`programming sage running on ${port}`);
 });
